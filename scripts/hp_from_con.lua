ce275fbc8c1505d4c263026290cc7f2b04a12c45
Wil Thieme <wil.thieme@protonmail.com>
2020-06-27 16:56:18 -0400

    Update hp_from_con.lua
    more work on intial buildout

--- scripts/hp_from_con.lua
+++ scripts/hp_from_con.lua
@@ -3,30 +3,22 @@
 --
 
 function onInit()
-	DB.addHandler(DB.getPath('charsheet.*.hp.*.total'), 'onUpdate', applyPenalties)
+	DB.addHandler(DB.getPath('charsheet.*.hp.total'), 'onUpdate', calculateTotalHp)
 end
 
---	Summary: Handles arguments of getHpFromCon()
+--	Summary: Handles arguments of calculateTotalHp()
 --	Argument: potentially nil node from triggering databasenode
---	Return: databasenode of PC and related rActor table
-local function handleGetHpFromConArgs(node)
+--	Return: node of player character under charsheet and related rActor table
+local function handlecalculateTotalHpArgs(node)
 	local nodePC
 	local rActor
 	
-	Debug.chat('Launch Node: ',node)
+--	Debug.chat('Launch Node: ',node)
 
-	if nodeField.getParent().getName() == 'charsheet' then
-		nodePC = nodeField
-	elseif nodeField.getName() == 'inventorylist' then
-		nodePC = nodeField.getParent()
-	elseif nodeField.getName() == 'hp' then
-		nodePC = nodeField.getParent()
-	elseif nodeField.getParent().getName() == 'inventorylist' then
-		nodePC = nodeField.getChild( '...' )
-	elseif nodeField.getName() == 'carried' then
-		nodePC = nodeField.getChild( '....' )
-	elseif nodeField.getName() == 'effects' then
-		rActor = ActorManager.getActor('ct', nodeField.getParent())
+	if node.getParent().getName() == 'hp' then
+		nodePC = node.getChild('...')
+	elseif node.getName() == 'effects' then
+		rActor = ActorManager.getActor('ct', node.getParent())
 		nodePC = DB.findNode(rActor['sCreatureNode'])
 	end
 
@@ -34,17 +26,29 @@ local function handleGetHpFromConArgs(node)
 		rActor = ActorManager.getActor("pc", nodePC)
 	end
 
+--	Debug.chat('Set Node: ', nodePC, rActor)
 	return nodePC, rActor
 end
 
 --	Summary: Recomputes penalties and updates max stat and check penalty
---	Arguments: nodeField - node of 'carried' when called from handler
-function getHpFromCon(node)
-	local nodePC, rActor = handleGetHpFromConArgs(node)
+--	Arguments: node - node of 'carried' when called from handler
+function calculateTotalHp(node)
+	local nodePC, rActor = handlecalculateTotalHpArgs(node)
+	local nConHP = getHpFromCon(nodePC)
+	
+	Debug.chat('nConHP', nConHP)
+	DB.setValue(nodePC, 'hp.conhp', 'number', nConHP)
+end
+
+--	Summary: gets total HP from CON
+--	Arguments: nodePC - node of player character under charsheet
+function getHpFromCon(nodePC)
 	local nCon = DB.getValue(nodePC, 'abilities.constitution.bonusmodifier', 0)
 	local nLevel = DB.getValue(nodePC, 'level', 0)
 	local nNegLevels = 0
 --	local nNegLevels = EffectManager35E.getEffectsBonus(rActor, 'NLVL', true)
 	local nConHP = nCon * (nLevel - nNegLevels)
-	Debug.chat(nCon, nLevel)
+
+	Debug.chat(nCon, '*', nLevel, '-', nNegLevels)
+	return nConHP
 end
