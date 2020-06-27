da65368fbec07d2b9e86f99c7104cfdd6060daec
Wil Thieme <wil.thieme@protonmail.com>
2020-06-27 18:35:33 -0400

    Update hp_from_con.lua
    reverse propogate level-up HP

--- scripts/hp_from_con.lua
+++ scripts/hp_from_con.lua
@@ -3,6 +3,7 @@
 --
 
 function onInit()
+	DB.addHandler(DB.getPath('charsheet.*.hp.total'), 'onUpdate', assimilateLevelHp)
 	DB.addHandler(DB.getPath('charsheet.*.hp.hdhp'), 'onUpdate', calculateTotalHp)
 	DB.addHandler(DB.getPath('charsheet.*.abilities.constitution.score'), 'onUpdate', calculateTotalHp)
 	DB.addHandler(DB.getPath('charsheet.*.abilities.constitution.bonus'), 'onUpdate', calculateTotalHp)
@@ -13,7 +14,7 @@ end
 --	Summary: Handles arguments of calculateTotalHp()
 --	Argument: potentially nil node from triggering databasenode
 --	Return: node of player character under charsheet and related rActor table
-local function handlecalculateTotalHpArgs(node)
+local function handleArgs(node)
 	local nodePC
 	local rActor
 --	Debug.chat('Launch Node: ',node)
@@ -22,6 +23,8 @@ local function handlecalculateTotalHpArgs(node)
 		nodePC = node.getChild('...')
 	elseif node.getParent().getName() == 'constitution' then
 		nodePC = node.getChild('....')
+	elseif node.getName() == 'level' then
+		nodePC = node.getParent()
 	elseif node.getName() == 'effects' then
 		rActor = ActorManager.getActor('ct', node.getParent())
 		nodePC = DB.findNode(rActor['sCreatureNode'])
@@ -38,7 +41,7 @@ end
 --	Summary: Recomputes penalties and updates max stat and check penalty
 --	Arguments: node - node of 'carried' when called from handler
 function calculateTotalHp(node)
-	local nodePC, rActor = handlecalculateTotalHpArgs(node)
+	local nodePC, rActor = handleArgs(node)
 	local nConHP = getHpFromCon(nodePC, rActor)
 	local nHDHP = DB.getValue(nodePC, 'hp.hdhp')
 	local nHPTotal = nConHP + nHDHP
@@ -77,4 +80,19 @@ function getConEffects(nodePC, rActor)
 	local nSpeedAdjFromEffects = EffectManager35E.getEffectsBonus(rActor, 'CON', true)
 
 	return nSpeedAdjFromEffects
+end
+
+--	Summary: Determine the total bonus to character's CON from effects
+--	Argument: rActor containing the PC's charsheet and combattracker nodes
+--	Return: total bonus to CON from effects formatted as 'CON: n' in the combat tracker
+function assimilateLevelHp(node)
+	local nodePC, rActor = handleArgs(node)
+	local nHDHP = DB.getValue(nodePC, 'hp.hdhp')
+	local nConHP = getHpFromCon(nodePC, rActor)
+	local nHPTotal = DB.getValue(nodePC, 'hp.total')
+
+	if nHPTotal ~= nHDHP + nConHP then
+		nHDHP = nHPTotal - nConHP
+		DB.setValue(nodePC, 'hp.hdhp', 'number', nHDHP)
+	end
 end
