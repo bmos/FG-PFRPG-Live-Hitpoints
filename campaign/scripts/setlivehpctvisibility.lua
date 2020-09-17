094c1d696b36d469d866319a9fc8db72be56c646
Wil Thieme <wil.thieme@protonmail.com>
2020-09-17 12:06:49 -0400

    fix for read-only total hp
    now the total only locks once the creature is added to the combat tracker

--- campaign/scripts/setlivehpctvisibility.lua
+++ campaign/scripts/setlivehpctvisibility.lua
@@ -5,6 +5,7 @@
 ---	This function is run when npc_combat_creature.bonushp is loaded
 function onInit()	
 	local bIsInCT = (window.getDatabaseNode().getParent().getName() == 'list')
+	window.hp.setReadOnly(bIsInCT)
 	window.hdhp.setVisible(bIsInCT)
 	window.hdhp_label.setVisible(bIsInCT)
 	window.bonushp.setVisible(bIsInCT)
