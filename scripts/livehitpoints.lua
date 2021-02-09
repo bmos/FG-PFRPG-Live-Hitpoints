7206a57360090b39ae84b9ebea3767dd570351cc
Wil Thieme <wil.thieme@protonmail.com>
2021-02-09 07:25:28 -0500

    initial implementation of Live HP for PCs

--- scripts/livehitpoints.lua
+++ scripts/livehitpoints.lua
@@ -2,21 +2,34 @@
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
 
----	This function calculates bonus HP from feats.
+---	This function returns the change in maximum hitpoints from effects.
+--	It checks for a hitpoint changes from "MHP: n" and reduces that by 5 for each negative level.
+function getEffectHp(rActor)
+	local nNegativeLevels = (EffectManager35EDS.getEffectsBonus(rActor, 'NLVL', true) * 5) or 0
+	local nMhp = EffectManager35EDS.getEffectsBonus(rActor, {'MHP'}, true) or 0
+	return nMhp - nNegativeLevels
+end
 
---	This function accepts triggers from PCLiveHP and NPCLiveHP.
-function calculateHp(nodeActor, rActor)
-	if not nodeActor or not rActor then
+---	This function accepts calls from PCLiveHP and NPCLiveHP.
+--	It then coordinates the functions in this script to ascertain total HP.
+function calculateHp(nodeActor, rActor, nAbilityBonus, nFeatBonus)
+	if not nodeActor or not rActor or not nAbilityBonus or not nFeatBonus then
 		return nil
 	end
-	
-	
+
+	local nRolledHp = DB.getValue(nodeActor, 'hp.rolled', 0)
+	local nMiscHp = DB.getValue(nodeActor, 'hp.misc', 0)
+	local nEffectHp = getEffectHp(rActor)
+
+	DB.setValue(nodeActor, 'hp.ability', 'number', nAbilityBonus)
+	DB.setValue(nodeActor, 'hp.feats', 'number', nFeatBonus)
+	DB.setValue(nodeActor, 'hp.total', 'number', nRolledHp + nAbilityBonus + nFeatBonus + nEffectHp + nMiscHp)
 end
 
 ---	This function checks whether an effect should trigger recalculation.
---	It does this by checking the effect text for a series of three letters followed by a colon (as used in bonuses like CON: 4).
+--	It does this by checking the effect text for a series of letters followed by a colon (as used in bonuses like CON: 4).
 function checkEffectRelevance(nodeEffect)
-	if string.find(DB.getValue(nodeEffect, 'label', ''), '%a%a%a:') then
+	if string.find(DB.getValue(nodeEffect, 'label', ''), '%a+:') then
 		return true
 	end
 	
--- scripts/livehitpoints_pc.lua
+++ scripts/livehitpoints_pc.lua
@@ -2,13 +2,93 @@
 --	Please see the LICENSE.md file included with this distribution for attribution and copyright information.
 --
 
+--
+--	PC Specific Data Acquisition Functions
+--
+
+---	This function checks PCs for feats, traits, and special abilities.
+function hasSpecialAbility(nodeActor, sSearchString, bFeat, bTrait, bSpecialAbility)
+	if not nodeActor then
+		return false;
+	end
+
+	local sLowerSpecAbil = string.lower(sSearchString);
+	if bFeat then
+		for _,vNode in pairs(DB.getChildren(nodeActor, '.featlist')) do
+			local vLowerSpecAbilName = StringManager.trim(DB.getValue(vNode, 'name', ''):lower());
+			local nRank = tonumber(vLowerSpecAbilName:match(sLowerSpecAbil .. ' (%d+)', 1));
+			if vLowerSpecAbilName and (nRank or vLowerSpecAbilName:match(sLowerSpecAbil, 1)) then
+				return true, (nRank or 1);
+			end
+		end
+	end
+	if bTrait then
+		for _,vNode in pairs(DB.getChildren(nodeActor, '.traitlist')) do
+			local vLowerSpecAbilName = StringManager.trim(DB.getValue(vNode, 'name', ''):lower());
+			local nRank = tonumber(vLowerSpecAbilName:match(sLowerSpecAbil .. ' (%d+)', 1));
+			if vLowerSpecAbilName and (nRank or vLowerSpecAbilName:match(sLowerSpecAbil, 1)) then
+				return true, (nRank or 1);
+			end
+		end
+	end
+	if bSpecialAbility then
+		for _,vNode in pairs(DB.getChildren(nodeActor, '.specialabilitylist')) do
+			local vLowerSpecAbilName = StringManager.trim(DB.getValue(vNode, 'name', ''):lower());
+			local nRank = tonumber(vLowerSpecAbilName:match(sLowerSpecAbil .. ' (%d+)', 1));
+			if vLowerSpecAbilName and (nRank or vLowerSpecAbilName:match(sLowerSpecAbil, 1)) then
+				return true, (nRank or 1);
+			end
+		end
+	end
+
+	return false
+end
+
+function getAbilityBonusUsed(nodePC, rActor, nLevel)
+	--update old data format to new format
+	local oldValue = DB.getValue(nodePC, 'hp.statused')
+	if oldValue then DB.deleteNode(nodePC.getChild('hp.statused')); DB.setValue(nodePC, 'hp.abilitycycler', 'string', oldValue) end
+	
+	local sAbility = DB.getValue(nodePC, 'hp.abilitycycler', 'constitution')
+	local nAbilityMod = DB.getValue(nodePC, 'abilities.' .. sAbility .. '.bonus', 0)
+	local nAbilityBonus = DB.getValue(nodePC, 'abilities.' .. sAbility .. '.bonusmodifier', 0)
+	local nAbilityDamage = math.floor(DB.getValue(nodePC, 'abilities.' .. sAbility .. '.damage', 0) / 2)
+	local nEffectBonus = math.floor((EffectManager35EDS.getEffectsBonus(rActor, {'CON'}, true) or 0) / 2)
+
+	return ((nAbilityMod + nAbilityBonus - nAbilityDamage + nEffectBonus) * nLevel) or 0
+end
+
+function getFeatBonusHp(nodePC, rActor, nLevel)
+	local nFeatBonus = 0
+	if DataCommon.isPFRPG() then
+		if hasSpecialAbility(nodePC, "Toughness", true) then
+			return nFeatBonus + math.max(nLevel, 3)
+		end
+	else
+		if hasSpecialAbility(nodePC, "Toughness", true) then
+			nFeatBonus = nFeatBonus + 3
+		end
+		if hasSpecialAbility(nodePC, "Improved Toughness", true) then
+			nFeatBonus = nFeatBonus + nLevel
+		end
+		return nFeatBonus
+	end
+	return 0
+end
+
+--
+--	Triggering Functions
+--
+
 ---	This function is called when effect components are changed.
 --	First, it checks if the triggering actor is a PC and whether the effect is relevant to this extension.
 --	Then, it calls the calculateHp function in LiveHP and provides it with nodeActor and rActor.
 local function onEffectChanged(node)
 	local rActor = ActorManager.resolveActor(node.getChild('....'))
 	if ActorManager.isPC(rActor) and LiveHP.checkEffectRelevance(node.getChild('..')) then
-		LiveHP.calculateHp(node.getChild('....'), rActor)
+		local nodePC = ActorManager.getCreatureNode(rActor)
+		local nLevel = DB.getValue(nodePC, 'level', 0)
+		LiveHP.calculateHp(nodePC, rActor, getAbilityBonusUsed(nodePC, rActor, nLevel), getFeatBonusHp(nodePC, rActor, nLevel))
 	end
 end
 
@@ -17,14 +97,28 @@ end
 local function onEffectRemoved(node)
 	local rActor = ActorManager.resolveActor(node.getChild('..'))
 	if ActorManager.isPC(rActor) then
-		LiveHP.calculateHp(ActorManager.getCreatureNode(rActor), rActor)
+		local nodePC = ActorManager.getCreatureNode(rActor)
+		local nLevel = DB.getValue(nodePC, 'level', 0)
+		LiveHP.calculateHp(nodePC, rActor, getAbilityBonusUsed(nodePC, rActor, nLevel), getFeatBonusHp(nodePC, rActor, nLevel))
 	end
 end
 
 ---	This function is called when ability score components are changed.
 --	It calls the calculateHp function in LiveHP and provides it with nodeActor and rActor.
 local function onAbilityChanged(node)
-	LiveHP.calculateHp(node.getChild('....'), ActorManager.resolveActor(node.getChild('....')))
+	local rActor = ActorManager.resolveActor(node.getChild('....'))
+	local nodePC = ActorManager.getCreatureNode(rActor)
+	local nLevel = DB.getValue(nodePC, 'level', 0)
+	LiveHP.calculateHp(nodePC, rActor, getAbilityBonusUsed(nodePC, rActor, nLevel), getFeatBonusHp(nodePC, rActor, nLevel))
+end
+
+---	This function is called when feats are added or renamed.
+--	It calls the calculateHp function in LiveHP and provides it with nodeActor and rActor.
+local function onFeatsChanged(node)
+	local rActor = ActorManager.resolveActor(node.getChild('....'))
+	local nodePC = ActorManager.getCreatureNode(rActor)
+	local nLevel = DB.getValue(nodePC, 'level', 0)
+	LiveHP.calculateHp(nodePC, rActor, getAbilityBonusUsed(nodePC, rActor, nLevel), getFeatBonusHp(nodePC, rActor, nLevel))
 end
 
 ---	This function watches for changes in the database and triggers various functions.
@@ -34,6 +128,8 @@ function onInit()
 		DB.addHandler(DB.getPath('charsheet.*.abilities.*.bonus'), 'onUpdate', onAbilityChanged)
 		DB.addHandler(DB.getPath('charsheet.*.abilities.*.bonusmodifier'), 'onUpdate', onAbilityChanged)
 		DB.addHandler(DB.getPath('charsheet.*.abilities.*.damage'), 'onUpdate', onAbilityChanged)
+		
+		DB.addHandler(DB.getPath('charsheet.*.featlist.*.name'), 'onUpdate', onFeatsChanged)
 
 		DB.addHandler(DB.getPath('combattracker.list.*.effects.*.label'), 'onUpdate', onEffectChanged)
 		DB.addHandler(DB.getPath('combattracker.list.*.effects.*.isactive'), 'onUpdate', onEffectChanged)
